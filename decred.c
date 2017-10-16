29aa5930e31d1ced6c0b8d6bff89436d9fd05e1e
hzeyoo <hzeyoo@gmail.com>
2017-10-16 15:10:48 +0800

    Create decred.c

---
+++ decred.c
@@ -0,0 +1,35 @@
+#include "blake.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "sha3/sph_blake.h"
+
+void blake_hash(const char* input, char* output, uint32_t len)
+{
+	sph_blake256_context ctx_blake;
+
+	sph_blake256_set_rounds(14);
+
+	sph_blake256_init(&ctx_blake);
+	sph_blake256(&ctx_blake, input, len);
+	sph_blake256_close(&ctx_blake, output);
+}
+
+static void hexlify(char *hex, const unsigned char *bin, int len)
+{
+	hex[0] = 0;
+	for(int i=0; i < len; i++)
+		sprintf(hex+strlen(hex), "%02x", bin[i]);
+}
+
+void decred_hash(const char* input, char* output, uint32_t len)
+{
+	sph_blake256_context ctx_blake;
+	sph_blake256_set_rounds(14);
+	if (len > 180) len  = 180;
+	sph_blake256_init(&ctx_blake);
+	sph_blake256(&ctx_blake, input, len);
+	sph_blake256_close(&ctx_blake, output);
+}
